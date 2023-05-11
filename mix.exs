a3b7836733573959b1fbb5ac08fa69890e6279c5
Yevhenii Molchanov <oocrazyb@gmail.com>
2023-05-11 12:18:17 +0300

    Add files via upload

---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule SimpleApp.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :simple_app,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {SimpleApp.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:tesla, "~> 1.4.0"},
+      {:hackney, "~> 1.16.0"},
+      {:jason, ">= 1.0.0"},
+      {:plug_cowboy, "~> 2.0"}
+    ]
+  end
+end
